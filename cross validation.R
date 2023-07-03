3587030951133a1b6f9ad920bb4fae9f61916eb1
Sanchayan Bhowal <sanchayan.bhowal2509@gmail.com>
2023-07-03 14:14:23 -0400

    Cross Validation added

--- cross validation.R
+++ cross validation.R
@@ -2,4 +2,8 @@ library(caret)
 ctrl <- trainControl(method = "cv", number = 10)
 
 #fit a regression model and use k-fold CV to evaluate performance
-cv_model <- train(g(Y) ~ ., data = pathway_surv1, method = "lm", trControl = ctrl)
+cv_model_path1 <- train(g(Y) ~ ., data = pathway_surv1, method = "lm", trControl = ctrl)
+cv_model_path2 <- train(g(Y) ~ ., data = pathway_surv2, method = "lm", trControl = ctrl)
+
+cv_model_pc1 <- train(g(Y) ~ ., data = pc_surv1, method = "lm", trControl = ctrl)
+cv_model_pc2 <- train(g(Y) ~ ., data = pc_surv2, method = "lm", trControl = ctrl)
