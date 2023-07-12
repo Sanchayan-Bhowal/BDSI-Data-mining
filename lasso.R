40dbd5d0564e42d7eadc3950178186b7b75add99
Sanchayan Bhowal <sanchayan.bhowal2509@gmail.com>
2023-07-12 16:08:47 -0400

    lasso,rf,lm,bt implemented with stability

--- lasso.R
+++ lasso.R
@@ -33,29 +33,34 @@ scores <- rbind(scores,path_naive_sum)
 library(glmnet)
 
 N <- 1000
-all_coefs <- c()
-for (i in 1:1000) {
-  set.seed(i)
-  if(i %% 100==0){
-    print(i/100)
+all_coefs_path <- c()
+for (j in 1:N) {
+  set.seed(j)
+  if(j %% 100==0){
+    print(j/100)
   }
+  lasso_keep <- sample(1:49,39)
+  P_lasso <- P_train[lasso_keep]
+  pathway_surv_lasso <- pathway_surv_train[lasso_keep,]
+  pathway.scores_lasso <- pathway.scores_train[lasso_keep,]
   
-  lasso <- cv.glmnet(pathway.scores_train,P_train,alpha=1)
-  best_lambda <- lasso$lambda.min
+  set.seed(07272023)
+  lasso_path <- cv.glmnet(pathway.scores_lasso,P_lasso,alpha=1)
+  best_lambda_path <- lasso_path$lambda.min
   
-  lasso <- glmnet(pathway.scores_train,P_train,alpha = 1,lambda = best_lambda)
-  coef_lasso <- coef(lasso)
-  all_coefs <- c(all_coefs,row.names(coef_lasso)[coef_lasso@i])
+  lasso_path <- glmnet(pathway.scores_train,P_train,alpha = 1,lambda = best_lambda_path)
+  coef_lasso_path <- coef(lasso_path)
+  all_coefs_path <- c(all_coefs_path,row.names(coef_lasso_path)[coef_lasso_path@i])
 }
-coefs_lasso=as.data.frame(all_coefs)
-ggplot(coefs_lasso,aes(x = all_coefs)) +
+coefs_lasso_path=as.data.frame(all_coefs_path)
+ggplot(coefs_lasso_path,aes(x = all_coefs_path)) +
   geom_bar()
-coefs_lasso <- coefs_lasso %>% count(all_coefs)
-frac <- 0.1 #fraction wanted
-coefs_lasso <- coefs_lasso %>% filter(n>(N*frac),all_coefs!="(Intercept)")
+coefs_lasso_path <- coefs_lasso_path %>% count(all_coefs_path)
+frac <- 0.2 #fraction wanted
+coefs_lasso_path <- coefs_lasso_path %>% filter(n>(N*frac),all_coefs_path!="(Intercept)")
 
 formula_pathway_lasso <- as.formula(paste("P ~", 
-                 paste(coefs_lasso$all_coefs, 
+                 paste(coefs_lasso_path$all_coefs_path, 
                        collapse = "+")))
 path_lasso<-train(formula_pathway_lasso, data = pathway_surv_train,
                    method = 'glmnet', 
@@ -64,5 +69,46 @@ path_lasso<-train(formula_pathway_lasso, data = pathway_surv_train,
                    metric = "RMSE"
 )
 predictions_path_lasso <- path_lasso %>% predict(pathway_surv_test)
+
+path_lasso_sum = postResample(predictions_path_lasso, P_test)
+
+N <- 1000
+all_coefs_pc <- c()
+for (j in 1:N) {
+  set.seed(j)
+  if(j %% 100==0){
+    print(j/100)
+  }
+  lasso_keep <- sample(1:49,39)
+  P_lasso <- P_train[lasso_keep]
+  pc_surv_lasso <- pc_surv_train[lasso_keep,]
+  pc_scores_lasso <- pc_scores_train[lasso_keep,]
+  pc_scores_lasso <- as.matrix(pc_scores_lasso)
+  
+  set.seed(07272023)
+  lasso_pc <- cv.glmnet(pc_scores_lasso,P_lasso,alpha=1)
+  best_lambda_pc <- lasso_pc$lambda.min
+  
+  lasso_pc <- glmnet(pc_scores_train,P_train,alpha = 1,lambda = best_lambda_pc)
+  coef_lasso_pc <- coef(lasso_pc)
+  all_coefs_pc <- c(all_coefs_pc,row.names(coef_lasso_pc)[coef_lasso_pc@i])
+}
+coefs_lasso_pc=as.data.frame(all_coefs_pc)
+ggplot(coefs_lasso_pc,aes(x = all_coefs_pc)) +
+  geom_bar()
+coefs_lasso_pc <- coefs_lasso_pc %>% count(all_coefs_pc)
+frac <- 0.2 #fraction wanted
+coefs_lasso_pc <- coefs_lasso_pc %>% filter(n>(N*frac),all_coefs_pc!="(Intercept)")
+
+formula_pc_lasso <- as.formula(paste("P ~", 
+                 paste(coefs_lasso_pc$all_coefs_pc, 
+                       collapse = "+")))
+path_lasso<-train(formula_pc_lasso, data = pc_surv_train,
+                   method = 'glmnet', 
+                   tuneGrid = expand.grid(alpha = 0, lambda = parameters),
+                   trControl = ctrl,
+                   metric = "RMSE"
+)
+predictions_path_lasso <- path_lasso %>% predict(pc_surv_test)
 
 path_lasso_sum = postResample(predictions_path_lasso, P_test)
---
+++ lm_funcs.R
@@ -0,0 +1,83 @@
+library(caret)
+
+N <- 10
+rf_coefs_path <- c()
+control <- rfeControl(functions=lmFuncs, method="cv", number=10)
+for (i in 1:N) {
+  set.seed(i)
+  if(i %% 1==0){
+    print(i/1)
+  }
+  lasso_keep <- sample(1:49,39)
+  P_lasso <- P_train[lasso_keep]
+  pathway_surv_lasso <- pathway_surv_train[lasso_keep,]
+  pathway.scores_lasso <- pathway.scores_train[lasso_keep,]
+  
+  set.seed(07272023)
+  #Random Forests
+  # run the RFE algorithm
+  results_pathway_rf <- rfe(pathway.scores_train, P_train, sizes=c(1:30), rfeControl=control)
+  # list the chosen features
+  
+  rf_coefs_path <- c(rf_coefs_path,predictors(results_pathway_rf))
+}
+coefs_rf_path=as.data.frame(rf_coefs_path)
+ggplot(coefs_rf_path,aes(x = rf_coefs_path)) +
+  geom_bar()
+coefs_rf_path <- coefs_rf_path %>% count(rf_coefs_path)
+frac <- 0.2 #fraction wanted
+coefs_rf_path <- coefs_rf_path %>% filter(n>(N*frac),rf_coefs_path!="(Intercept)")
+
+formula_pathway_rf <- as.formula(paste("P ~", 
+                                          paste(coefs_rf_path$rf_coefs_path, 
+                                                collapse = "+")))
+path_rf<-train(formula_pathway_rf, data = pathway_surv_train,
+                  method = 'glmnet', 
+                  tuneGrid = expand.grid(alpha = 0, lambda = parameters),
+                  trControl = ctrl,
+                  metric = "RMSE"
+)
+predictions_path_rf <- path_rf %>% predict(pathway_surv_test)
+
+path_rf_sum = postResample(predictions_path_rf, P_test)
+
+N <- 10
+rf_coefs_pc <- c()
+control <- rfeControl(functions=lmFuncs, method="cv", number=10)
+for (i in 1:N) {
+  set.seed(i)
+  if(i %% 1==0){
+    print(i/1)
+  }
+  lasso_keep <- sample(1:49,39)
+  P_lasso <- P_train[lasso_keep]
+  pc_surv_lasso <- pc_surv_train[lasso_keep,]
+  pc_scores_lasso <- pc_scores_train[lasso_keep,]
+  
+  set.seed(07272023)
+  #Random Forests
+  # run the RFE algorithm
+  results_pc_rf <- rfe(pc_scores_train, P_train, sizes=c(1:30), rfeControl=control)
+  # list the chosen features
+  
+  rf_coefs_pc <- c(rf_coefs_pc,predictors(results_pc_rf))
+}
+coefs_rf_pc=as.data.frame(rf_coefs_pc)
+ggplot(coefs_rf_pc,aes(x = rf_coefs_pc)) +
+  geom_bar()
+coefs_rf_pc <- coefs_rf_pc %>% count(rf_coefs_pc)
+frac <- 0.2 #fraction wanted
+coefs_rf_pc <- coefs_rf_pc %>% filter(n>(N*frac),rf_coefs_pc!="(Intercept)")
+
+formula_pc_rf <- as.formula(paste("P ~", 
+                                          paste(coefs_rf_pc$rf_coefs_pc, 
+                                                collapse = "+")))
+pc_rf<-train(formula_pc_rf, data = pc_surv_train,
+                  method = 'glmnet', 
+                  tuneGrid = expand.grid(alpha = 0, lambda = parameters),
+                  trControl = ctrl,
+                  metric = "RMSE"
+)
+predictions_pc_rf <- pc_rf %>% predict(pc_surv_test)
+
+pc_rf_sum = postResample(predictions_pc_rf, P_test)
